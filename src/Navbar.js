4a49d51f0645dcfb580e552e178f9273ccf638d6
clab <aden.li.doudou@gmail.com>
2024-08-08 10:38:15 -0700

    Add files via upload
    new src thing

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,17 +3,22 @@ import {Link} from 'react-router-dom'
 
 import "./Navbar.css"
 
+import gimage from "./Pages/Images/Google__G__logo.svg.png"
+
 export default function Navbar(){
     return(
         <>
             <div id="navbar">
-                <div id="navbar_logo"/>
+                <img id="navbar_logo" src={gimage}/>{/*  />*/}
                 <ul id="navbar_links">
                     <li className="navbar_link">
-                        <Link to='/'><p className='navbar_font'>Example</p></Link>
+                        <Link to='/'><p className='navbar_font'>Bisney Blus</p></Link>
                     </li>
                     <li className="navbar_link">
-                        <Link to='/exampleone'><p className='navbar_font'>Example one</p></Link>
+                        <Link to='/exampleone'><p className='navbar_font'>Google Premium</p></Link>
+                    </li>
+                    <li className="navbar_link">
+                        <Link to='/racegame'><p className='navbar_font'>race game</p></Link>
                     </li>
                 </ul>
             </div>
--- src/Pages/ExampleOne.js
+++ src/Pages/ExampleOne.js
@@ -4,12 +4,15 @@ import "./ExampleOne.css"
 
 import Navbar from "../Navbar";
 
+import image from "./Images/unnamed.png"
 export default function ExampleOne(){
     return(
         <>
             <Navbar />
             <div id="exampleone">
-
+<a href="https://www.w3schools.com"><img id="exampleone_img1" src={image}/></a>
+<a href="https://www.youtube.com/watch"><img id="exampleone_img1" src={image}/></a>
+<a href="https://www.figma.com/design/bRoff6hjGeOxo9cJtAAcI2/Bisney%2B?node-id=73-2&t=yT5Uka53s95E9qMv-0"><img id="exampleone_img1" src={image}/></a>
             </div>
         </>
     )
